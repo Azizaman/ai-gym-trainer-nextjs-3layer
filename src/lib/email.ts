b2b09c8d5fb099a0145b2ad8dca6743a28c5ff76
azizaman <azizamanaaa97@gmail.com>
2026-03-01 00:32:17 +0530

    docker file updated3

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || "re_dummy_fallback_key");
 
 const APP_NAME = "FormAI";
 const FROM_EMAIL = process.env.EMAIL_FROM || "FormAI <noreply@support.formai.site>";
